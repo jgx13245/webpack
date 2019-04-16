f6d8820b22b44030d262f0a618d4ae62599a435f
冀光鑫 <>
2019-04-16 15:23:29 +0800

    :sparkles: plugin插件的基础使用

--- README.md
+++ README.md
@@ -1,38 +1,26 @@
-## css类型文件处理的基本配置参数 
- - 这次我们需要用到 css-loader，style-loader 等 loader，跟 babel 一样，webpack 不知道将 CSS 提取到文件中。需要使用 loader 来加载对应的文件 
-  
-  ### css-loader:负责解析 CSS 代码，主要是为了处理 CSS 中的依赖，例如 @import 和 url() 等引用外部文件的声明 
-  
-  ### style-loader 会将 css-loader 解析的结果转变成 JS 代码，运行时动态插入 style 标签来让 CSS 代码生效。 
+- webpack 里面使用loader可以完成不同类型文件的打包
+### plugins 更加快捷的打包
+1.场景一，打包生成的dist的目录下本来应该有个index.html的文件，但是是手动添加的。怎么解决这个东西呢？ 
+HtmlWebpackPlugin：插件就可以解决这个问题
+(1)可以在webpack打完包以后自动生成一个index.html的文件，但是每次打包都是改变的，会自动引入打包生成的bundle.js的文件，
 
-> 样式生效以后，不会生成一个css的文件夹，webpack会通过 style-laoder自动的吧样式加在头部的 style 里面
+(2)假如我们可能需要模板里面有个root的标签，我们就可以使用这个插件的配置选项，
+   ```
+   plugins: [new HtmlWebpackPlugin({
+    template:'./src/index.html'
+  })]
+   ```
+   我们可以在其他目录提前设置好一个模板，添加上这个标签，这样，自动打包生成的index.html的文件就会带有这个标签的
 
- 1、如果想要使用scss的后缀样式，就需要继续引入 sass-laoder  node-sass的两个loader.然后继续加在 use:['style-loader','css-loader']  后面. scss-laoder 可以让scss的后缀文件变成css的文件
- 
- 2.use:['style-loader','css-loader','scss-loader'] 这里面的执行顺序是  **从下到上。从左到右执行的**
+(3)plugins 可以在你webpack运行到某个时候的帮你做一些事，类似生命周期函数
 
- 3.postcss-laoder:有时候有些css3的新特性，必须加上浏览器前缀，才能兼容，就需要这个特性，
- (1)在根目录添加 postcss.config.js文件，里面设置一下，需要npm install autoprefixer的依赖，这样的css3 的特性就会加上 厂商前缀
-
-4.importLoaders 假如有这个场景，各种loader本来是解决js文件里面引入的不同类型的文件用来解析的，如果一个scss的文件里面通过@import引入了另一个scsd的文件，那么第二个scss的文件可能就不会用上post-laoder的转换，这就不好了，使用importloaders可是让嵌套的scss也把地下的loader也使用上
-
-## css模块化打包
-1.假如你在index.js里面引入了一个样式。另外还有个js的文件，也有自己的样式，可能就会影响和冲突，这样打包，就不会影响
-平时我们引用css文件手机 import './xx.scss'
-使用以后就可以模块化使用。import cssModule from './xx.scss'
-下面使用就可以 cssModule.xxxx
-```
-{
-      test:/\.scss$/,
-      use:['style-loader',
-           {
-             loader:'css-loader',
-             options:{
-               importLoaders:2,
-               module:true  // 开启样式模块化打包
-             }
-           },
-           'sass-loader',
-           'postcss-loader']
-    }
-```
+(4)clean-webpack-plugin：有时候我们打包好的js文件想改改名字，但是重新打包，dist的文件夹不会丢失，会冗余，所以打包前先把dist的文件夹删除，再重新打包，
+   这个插件可以实现这个功能
+   ```
+   plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html'
+    }),
+   new CleanWebpackPlugin() //  版本不同，参数不同，这个不用传参
+  ]
+   ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,49 @@
 const path = require('path')
-module.exports= {
-  mode:'development', // 区分是什么环境，，避免报错
-  entry:'./src/index.js', // 入口文件
-  output:{ //输出文件
-    filename:'app.js',
-    path:path.resolve(__dirname,'dist') // path 会在根目录生成一个dist的文件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+  mode: 'development', // 区分是什么环境，，避免报错
+  entry: './src/index.js', // 入口文件
+  output: { //输出文件
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist') // path 会在根目录生成一个dist的文件
   },
-  module:{ // 添加各种laoder  
+  // 添加各种laoder 
+  module: {
     /* url-loader和file-laoder差不多，但是url-loader不会把所有的图片打包到dist里面。会自动转成base64合成到app.js里面，有个属性，limit 
-    */
-    rules:[{
-      test:/\.(png|jpg)$/,
-      use:{
-        loader:'url-loader', // 解析不同的后缀名。图片的和后缀名，vue的是vue-loader
-        options:{ 
-          name:'[name].[ext]', // 占位符配置，不配置的话，dist里面的名字会有问题，这样就可以使其名字个后缀都是原来的配置，
-          outputPath:'images/',// 可以子啊dist里面生成文件夹，区分不同的文件
-          limit:2048  // 如果大于limit设置的大小，就单独生成图片，否则就直接加在js里面，减少http的请求
+     */
+    rules: [{
+        test: /\.(png|jpg)$/,
+        use: {
+          loader: 'url-loader', // 解析不同的后缀名。图片的和后缀名，vue的是vue-loader
+          options: {
+            name: '[name].[ext]', // 占位符配置，不配置的话，dist里面的名字会有问题，这样就可以使其名字个后缀都是原来的配置，
+            outputPath: 'images/', // 可以子啊dist里面生成文件夹，区分不同的文件
+            limit: 2048 // 如果大于limit设置的大小，就单独生成图片，否则就直接加在js里面，减少http的请求
+          }
         }
+      },
+      // css-loader部分解析
+      {
+        test: /\.scss$/,
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          'sass-loader',
+          'postcss-loader'
+        ]
       }
-    },
-    // css-loader部分解析
-    {
-      test:/\.scss$/,
-      use:['style-loader',
-           {
-             loader:'css-loader',
-             options:{
-               importLoaders:2
-             }
-           },
-           'sass-loader',
-           'postcss-loader']
-    }]
-  }
+    ]
+  },
+  // 插件部分
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html'
+    }),
+   new CleanWebpackPlugin()
+  ]
 }
