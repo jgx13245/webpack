9aa42d2754aac3e870f53e1f8ad185ac776dc957
冀光鑫 <>
2020-06-10 21:16:41 +0800

    fe

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@
 //webpack 模块打包工具
 // Es modules  方式   import a from './a'   export default = a;
 // Commonjs  node引入方式 var  a  = require('./sasa')      moudele.exports = a
-import "@babel/polyfill"
 
-const arr = [
-  new  Promise(() => {}),
-  new  Promise(() => {})
-]
+// index.js代码
 
-console.log(arr)
+var fn = x => x * x;
