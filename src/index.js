a14340a690a96a5baaa890d531672b34c8bfac34
冀光鑫 <>
2020-06-11 15:49:53 +0800

    f

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@
 //webpack 模块打包工具
 // Es modules  方式   import a from './a'   export default = a;
 // Commonjs  node引入方式 var  a  = require('./sasa')      moudele.exports = a
-import imgs  from './1.png'
 import './a.scss'
 
-var img = new Image();
-img.src = imgs
-img.classList.add('m')
+var div = document.createElement('div');
+div.classList.add('m')
 var root = document.getElementById('app')
-root.append(img)
+root.append(div)
 
