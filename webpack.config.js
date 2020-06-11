5785b32903682d295ad77422e34a4b4cb66513f9
冀光鑫 <>
2020-06-11 15:37:01 +0800

    ss

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports= {
         loader:'url-loader', // 解析不同的后缀名。图片的和后缀名，vue的是vue-loader
         options:{ 
           name:'[name].[ext]', // 占位符配置，不配置的话，dist里面的名字会有问题，这样就可以使其名字个后缀都是原来的配置，
-          outputPath:'images/',// 可以子啊dist里面生成文件夹，区分不同的文件
           limit:2048  // 如果大于limit设置的大小，就单独生成图片，否则就直接加在js里面，减少http的请求
         }
       }
